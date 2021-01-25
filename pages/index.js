95fe2ed35d8343aca9160ffba9a3f9a9b3c932a0
Abnner <abnnerdavi019@gmail.com>
2021-01-25 09:51:45 -0300

    Starter code in alura Immersion

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,56 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../source/components/Widget'
+import Footer from '../source/components/Footer'
+import GitHubCorner from '../source/components/GitHubCorner'
+import QuizBackground from '../source/components/QuizBackground'
+
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz da Unevale</h1>
+          </Widget.Header>
+
+          <Widget.Content>
+            <p>Lorem Ipsum sit Amet...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+        <Widget.Content>
+            <p>Meu Quiz</p>
+            <p>Lorem Ipsum sit Amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner/>
+    </QuizBackground>
+  )
 }
